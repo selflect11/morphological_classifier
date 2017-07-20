52329ab0ab528f46d5e15e4c47f4c11e1cfb8cca
Pedro <pedrovolpi@poli.ufrj.br>
2017-07-20 00:13:54 -0300

    New project, new tests

--- morphological_classifier/tests/tests_models.py
+++ morphological_classifier/tests/tests_models.py
@@ -5,29 +5,5 @@ from ..models import tags_from_array
 import numpy as np
 import unittest
 
-class TestTools(unittest.TestCase):
-    def test_closest_class(self):
-        in_out = [('ADJ', [0.99, 0,0.1,-0.1]),
-                ('ADV', [0.03, 0.989, -0.04, 0.0022]),
-                ('V', [0.01,-0.0049, 0.875, 0.001]),
-                ('N', [0,0,0,1])
-                ]
-        for answer, arr in in_out:
-            arr = np.array(arr)
-            tryout = tags_from_array(arr)
-            with self.subTest(tryout = tryout):
-                self.assertEqual(answer, tryout)
-    def test_update_progress(self):
-        up = tools.update_progress
-        paf = 'C:/Users/Volpi\\Google Drive\\TCC/morphological_classifier/data/macmorpho-mini.txt'
-        txt = Text()
-        with open(paf, 'r', encoding=constants.ENCODING) as f:
-            lines = f.readlines()
-            num_lines = len(lines)
-        for line_num, line in enumerate(lines):
-            percentage = line_num/(num_lines - 1)
-            txt.add_line(line)
-            up(percentage)
-        
 if __name__ == '__main__':
     unittest.main()
